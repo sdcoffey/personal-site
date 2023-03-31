81dbce8273b758d73a8f33e5a5a229dde835d9a2
sdcoffey <stevendcoffey@gmail.com>
2023-03-31 07:23:34 -0700

    pages and prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://stevendcoffey.com",
+  experimental: {
+    assets: true,
+  },
   markdown: {
     draft: true,
   },
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as BaseHead } from "./BaseHead.astro";
+export { default as CodeBlock } from "./CodeBlock.astro";
+export { default as Footer } from "./Footer.astro";
+export { default as FormattedDate } from "./FormattedDate.astro";
+export { default as HeaderLink } from "./HeaderLink.astro";
+export { default as If } from "./If.astro";
+export { default as ResumeCard } from "./ResumeCard.astro";
