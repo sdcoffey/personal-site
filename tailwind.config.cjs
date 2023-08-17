bb168036140a5d382fe5acce22e7e57c48d465ac
sdcoffey <stevendcoffey@gmail.com>
2023-08-17 13:38:47 -0700

    add foss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,32 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    function ({ addComponents }) {
+      addComponents({
+        ".image-noselect": {
+          "-webkit-touch-callout": "none !important",
+          "-webkit-user-select": "none !important",
+          "pointer-events": "none",
+        },
+        ".max-h-available": {
+          "max-height": "stretch",
+        },
+        ".min-h-available": {
+          "min-height": "stretch",
+        },
+        ".h-available": {
+          "min-height": "stretch",
+        },
+        ".scrollbar-hidden::-webkit-scrollbar": {
+          display: "none",
+        },
+        "scrollbar-hidden": {
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+        },
+      });
+    }
+  ],
 };
