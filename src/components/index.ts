ba3a26eb6c41a5b4b9696530a7135bff48692363
sdcoffey <stevendcoffey@gmail.com>
2023-08-16 22:19:48 -0700

    update nav

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as BaseHead } from "./BaseHead.astro";
 export { default as CodeBlock } from "./CodeBlock.astro";
 export { default as Footer } from "./Footer.astro";
+export { default as Nav } from './Nav.astro';
 export { default as FormattedDate } from "./FormattedDate.astro";
 export { default as HeaderLink } from "./HeaderLink.astro";
 export { default as If } from "./If.astro";
