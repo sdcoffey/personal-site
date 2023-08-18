12b5ee0c9b58fb45e0f8a0aa6193b7ca074b7e88
sdcoffey <stevendcoffey@gmail.com>
2023-08-17 21:32:31 -0700

    wip importmap

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,8 @@ export { default as BaseHead } from "./BaseHead.astro";
 export { default as BlogCard } from "./BlogCard.astro";
 export { default as CodeBlock } from "./CodeBlock.astro";
 export { default as Footer } from "./Footer.astro";
+export { default as Footnote } from "./Footnote.astro";
+export { default as Footnotes } from "./Footnotes.astro";
 export { default as FormattedDate } from "./FormattedDate.astro";
 export { default as HeaderLink } from "./HeaderLink.astro";
 export { default as If } from "./If.astro";
