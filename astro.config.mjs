9313d12666623a8106e6858e74bc5eb371ae05a4
sdcoffey <stevendcoffey@gmail.com>
2023-09-01 13:44:29 -0700

    upgrade to astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,9 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://stevendcoffey.com",
-  experimental: {
-    assets: true,
-  },
   integrations: [
     mdx({
       drafts: true,
@@ -19,3 +14,4 @@ export default defineConfig({
     tailwind(),
   ],
 });
+
