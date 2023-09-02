223ff493c1b235b019f8a689fe712e657c888c56
sdcoffey <stevendcoffey@gmail.com>
2023-09-02 10:12:37 -0700

    fix rootDIr

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,8 +1,18 @@
 import path from 'path';
+import fs from 'fs';
 import url from 'url'
 
+function isRoot(path: string): boolean {
+  return fs.readdirSync(path).includes('package.json');
+}
+
 export function projectRootPath(): string {
-  return path.resolve(url.fileURLToPath(import.meta.url), '..', '..', '..');
+  let currentDir = path.resolve(url.fileURLToPath(import.meta.url), '..');
+  while (!isRoot(currentDir)) {
+    currentDir = path.resolve(currentDir, '..');
+  }
+
+  return currentDir;
 }
 
 export function joinToProjectRoot(...parts: string[]): string {
