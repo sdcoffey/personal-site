b9352cb54561009bf16583b4cc287a422e922b12
sdcoffey <stevendcoffey@gmail.com>
2023-09-08 09:55:55 -0700

    replace clipboard with square-2-stack

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,3 @@
 export { default as Checkmark } from "./checkmark.astro";
 export { default as Clipboard } from "./clipboard.astro";
+export { default as Square2Stack } from "./square-2-stack.astro";
